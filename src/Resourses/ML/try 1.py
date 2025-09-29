5e6210e6285edf6dcee4f894456fa71f1256c4dc
Divy Bhanushali <bhanushalidivy1@gmail.com>
2025-09-29 14:58:27 -0400

    Initial commit: Semester 7 ML notebooks/scripts with README and gitignore

---
+++ src/Resourses/ML/try 1.py
@@ -0,0 +1,29 @@
+a=int(input("Enter first value"))
+b=int(input("Enter second value"))
+
+#arithemtic operator
+print("\nArithmetic Operator")
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+#relational operator
+print("\nRelational Operator")
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+#logical operator
+print("\nLogical Operators")
+print(a<5 and a>10)
+print(a<5 and a<10)
+print(a>5 or a<2)
+print(a<2 or a<10)
+print(not(a>5 and a>10))
